1317398c2b45d03a0c1baff5e8b4720c858e5716
Rafael Salguero <rafaelsalgueroiturrios@gmail.com>
2017-11-23 15:53:40 -0700

    rxToReact

--- src/rxToReact.tsx
+++ src/rxToReact.tsx
@@ -1,14 +1,13 @@
 import { Observable } from "rxjs/Observable"
 import * as React from "react";
-import { componentToRx } from "./component2rx";
+import { componentToRx, ComponentToRxPropOptions, ReactComponent } from "./component2rx";
 
 
-const Dummy = (props: { comp: JSX.Element | null }) => props.comp || null;
-const DummyRx = componentToRx(Dummy);
+const dummy = (props: { comp: JSX.Element | null }) => props.comp || null;
 /**
  * Convert an observable of JSX.Element on to a JSX.Element
  * @param observable A stream of JSX.Elements
  */
-export function rxToReact(observable: Observable<JSX.Element | null> | PromiseLike<JSX.Element | null>) {
-    return <DummyRx comp={observable} />
+export function rxToReact(observable: Observable<JSX.Element | null> | PromiseLike<JSX.Element | null>, fallback?: JSX.Element, error?: ReactComponent<{ error: string }>) {
+    return componentToRx(dummy, fallback, error);
 }
